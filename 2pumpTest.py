653b30e2314e9707293c0c9512760035c9da7b1f
bmiles <ben.miles2@gmail.com>
2014-09-18 16:10:37 +0100

    2pumptest got retracked

---
+++ 2pumpTest.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('')
+import pumpy
+
+chain = pumpy.Chain('../../../../../dev/ttyUSB0')
+phd = pumpy.PHD2000(chain,address=1, name="Pump1") # PHD2000
+phd2 = pumpy.PHD2000(chain,address=12, name="Pump2") # special pump
+
+phd2.setdiameter(25)
+phd.setdiameter(25)
+
+phd.setflowrate(600)
+phd2.setflowrate(600)
+
+phd.settargetvolume(200)
+phd2.settargetvolume(200)
+
+phd.infuse()
+phd.stop()
+phd2.infuse()
+phd2.stop()
+
+sys.exit()
