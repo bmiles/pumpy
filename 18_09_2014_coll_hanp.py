20c8c0d96ba0155207c422a55047ff3bf64d3fa6
bmiles <ben.miles2@gmail.com>
2014-09-29 18:59:30 +0100

    added push notifiations

--- 18_09_2014_coll_hanp.py
+++ 18_09_2014_coll_hanp.py
@@ -2,17 +2,35 @@ import sys
 import pumpy
 import logging
 import time
+import os
+# Push notification stuff
 
-from progress.bar import ChargingBar
+import httplib, urllib
 
-def infuseProg(flowRate, targetVol):
-  totalDur = ((1/flowRate)*targetVol) * 60
-  delayDur = totalDur/20
-  bar = ChargingBar('Infusing', max=20)
-  for i in range(20):
-      time.sleep(delayDur)
-      bar.next()
-  bar.finish()
+pushover_user_key = os.environ['PUSHOVER_USER_KEY']
+pushover_app_key = os.environ['PUSHOVER_PUMPY_APP_TOKEN']
+
+
+def push(message):
+  conn = httplib.HTTPSConnection("api.pushover.net:443")
+  conn.request("POST", "/1/messages.json",
+    urllib.urlencode({
+      "token": pushover_user_key,
+      "user": pushover_app_key,
+      "message": message,
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+  conn.getresponse()
+
+# from progress.bar import ChargingBar
+#
+# def infuseProg(flowRate, targetVol):
+#   totalDur = ((1/flowRate)*targetVol) * 60
+#   delayDur = totalDur/20
+#   bar = ChargingBar('Infusing', max=20)
+#   for i in range(20):
+#       time.sleep(delayDur)
+#       bar.next()
+#   bar.finish()
 
 
 logging.basicConfig(level=logging.INFO)
@@ -21,25 +39,29 @@ chain = pumpy.Chain('../../../../../dev/ttyUSB0')
 PHDcoll = pumpy.PHD2000(chain,address=1, name="PHDcollagen") # PHD2000
 PHDha = pumpy.PHD2000(chain,address=12, name="PHDhaNP") # special pump
 
+tVol = 2000 # 2mL
+tTime = 30 # 60 minutes
+flowRate = tVol/tTime
+cycles = 20
+
 # Set diameters
 PHDcoll.setdiameter(25)
 PHDha.setdiameter(25)
 # Set Flow Rates
-PHDcoll.setflowrate(600)
-PHDha.setflowrate(600)
+PHDcoll.setflowrate(flowRate)
+PHDha.setflowrate(flowRate)
 # Set each target volume for each infuse.
-PHDcoll.settargetvolume(2000)
-PHDha.settargetvolume(2000)
+PHDcoll.settargetvolume(tVol)
+PHDha.settargetvolume(tVol)
 
 
-for i in range(0,20):
+for i in range(0,cycles):
+  push("Starting cycle" + stri(i))
   PHDcoll.infuse()
   logging.info('Collagen: infusing, cycle ' + str(i))
-  infuseProg(600, 2000)
   PHDcoll.stop()
   logging.info('Collagen: stopped infusing, cycle ' + str(i))
   PHDha.infuse()
-  infuseProg(600, 2000)
   logging.info('HAnp: infusing, cycle ' + str(i))
   PHDha.stop()
   logging.info('HAnp: stopped infusing, cycle ' + str(i))
